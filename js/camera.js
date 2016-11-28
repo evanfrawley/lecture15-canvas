98c2c326764e7e052bf07bdc640d03e7f4291844
Joel Ross <joelross@uw.edu>
2016-11-28 10:29:31 -0800

    End of lecture code

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,59 @@
 'use strict';
 
-//YOUR CODE GOES HERE
+var video = document.querySelector('video');
+var canvas = document.querySelector('#canvas');
+var brush = canvas.getContext('2d');
+
+navigator.getUserMedia = navigator.getUserMedia || 
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia;
+
+var playingStream;
+
+//how to call
+document.querySelector('#record').addEventListener('click',() =>{
+    navigator.getUserMedia( {video:{mandatory: {maxWidth:300, maxHeight:300}}}, 
+    function(mediaStream) {
+        playingStream = mediaStream; //GLOBAL!
+
+        video.src = window.URL.createObjectURL(mediaStream)        
+
+    //mediaStream is of type MediaStream
+    //do something with it here!
+        console.log("you have the camera!");
+    }, function(err) {
+        console.log(err);
+    });
+})
+
+document.querySelector('#stop').addEventListener('click', () => {
+    video.pause();
+    var tracks = playingStream.getTracks();
+    tracks.forEach(function(track){
+        track.stop(); //stop each track
+    });
+});
+
+document.querySelector('#capture').addEventListener('click', () => {
+    if(playingStream){
+        canvas.width = video.clientWidth; //note `clientWidth`
+        canvas.height = video.clientHeight;
+
+        //draw the video on the canvas as an image!
+        brush.drawImage(video,0,0);
+    }
+});
+
+document.querySelector('#save').addEventListener('click', () => {
+    var snapshot = canvas.toDataURL('image/png');
+    console.log(snapshot);
+
+    var link = document.createElement('a');
+    link.href = snapshot;
+    link.download = 'selfie.png';
+
+    link.click();
+
+
+});
--- js/drawing.js
+++ js/drawing.js
@@ -1,3 +1,27 @@
 'use strict';
 
-//YOUR CODE GOES HERE
+var canvas = document.querySelector('#canvas');
+var brush = canvas.getContext('2d');
+
+var lastLocation = undefined;
+canvas.addEventListener('mousedown', function(event){
+    var input = document.querySelector('input');
+    brush.strokeStyle = input.value;
+    brush.beginPath();
+    brush.moveTo(event.offsetX, event.offsetY);
+    //brush.arc(event.offsetX, event.offsetY, 3, 0, 2*Math.PI);
+    //brush.fill();
+    lastLocation = {x:event.offsetX, y:event.offsetY};
+});
+
+canvas.addEventListener('mousemove', function(event){
+    if(lastLocation){
+        brush.lineTo(event.offsetX, event.offsetY);
+        brush.stroke();
+    }
+});
+
+canvas.addEventListener('mouseup', function() {
+    lastLocation = undefined; //no longer pressing
+})
+
