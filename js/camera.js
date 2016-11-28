2615235979efabf24c2b2f7d73a933cfa7486967
Joel Ross <joelross@uw.edu>
2016-11-28 12:24:32 -0800

    End of lecture code

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,53 @@
 'use strict';
 
-//YOUR CODE GOES HERE
+navigator.getUserMedia = navigator.getUserMedia || 
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia;
+//navigator.getUserMedia(); //will use whichever available!
+
+var video = document.querySelector('video');
+var canvas = document.querySelector('#canvas');
+var brush = canvas.getContext('2d');
+var playingStream;
+
+document.querySelector('#record').addEventListener('click', () => {
+    navigator.getUserMedia({video:{mandatory: {maxWidth:300, maxHeight:300}}}, 
+    function(mediaStream) {
+        playingStream = mediaStream; //save for later
+
+        //set the src to be the stream
+        video.src = window.URL.createObjectURL(mediaStream)
+    
+
+    }, function(err){ 
+        console.log(err); 
+    });
+})
+
+document.querySelector('#stop').addEventListener('click', () => {
+    video.pause();
+    var tracks = playingStream.getTracks();
+    tracks.forEach(function(track){
+        track.stop(); //stop each track
+    });
+});
+
+document.querySelector('#capture').addEventListener('click', () => {
+    if(playingStream){
+        canvas.width = video.clientWidth; //note `clientWidth`
+        canvas.height = video.clientHeight;        
+
+        //draw the video on the canvas as an image!
+        brush.drawImage(video,0,0);        
+    }
+});
+
+document.querySelector('#save').addEventListener('click', () => {
+    var snapshot = canvas.toDataURL('image/png');
+
+    var link = document.createElement('a');
+    link.href = snapshot;
+    link.download = 'selfie-b.png';
+    link.click();
+});
--- js/drawing.js
+++ js/drawing.js
@@ -1,3 +1,55 @@
 'use strict';
 
-//YOUR CODE GOES HERE
+var canvas = document.querySelector('#canvas');
+var brush = canvas.getContext('2d');
+
+// //dip in color
+brush.fillStyle = "#39275B";
+brush.strokeStyle = "#39275B"
+brush.lineWidth = 3;
+
+// //draw a rect
+// brush.fillRect(30,40, 150, 250);
+// brush.strokeRect(30,40, 150, 250);
+
+// brush.fillRect(300,300,20,40);
+
+// //draw a path
+// brush.beginPath();
+// brush.moveTo(100,100);
+// brush.lineTo(200,300);
+// brush.lineTo(50,200);
+// brush.lineTo(100,200);
+// brush.closePath();
+
+// brush.fillStyle = "#39275B";
+// brush.stroke();
+// brush.fill();
+
+// //circle
+// brush.beginPath();
+// brush.arc(300,200, 60, 0, 2*Math.PI);
+// brush.fill();
+
+var drawing = false;
+
+canvas.addEventListener('mousedown', (event) => {
+    brush.beginPath();
+    //color based on input
+    brush.strokeStyle = document.querySelector('input').value;
+    brush.moveTo(event.offsetX, event.offsetY);
+    drawing = true;
+});
+
+canvas.addEventListener('mousemove', (event) => {
+    if(drawing){
+        brush.lineTo(event.offsetX, event.offsetY);
+        brush.stroke();     
+    }
+});
+
+canvas.addEventListener('mouseup', () => { 
+    drawing = false;
+})
+
+
